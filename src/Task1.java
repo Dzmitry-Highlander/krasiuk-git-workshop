91489db44a4626a89bcab6d4752ee28feefae15c
Olga Komlik <oluska92666@gmail.com>
2022-09-21 13:00:37 +0300

    fixed file

--- src/Task1.java
+++ src/Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello Dima!");
-        System.out.println("DADADA");
+        System.out.println("where is conflict");
     }
 }

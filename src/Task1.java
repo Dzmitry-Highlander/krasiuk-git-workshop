6cc3d58b6de68e8a858c390dc0abafb97e5bc12c
Olga Komlik <oluska92666@gmail.com>
2022-09-21 13:44:48 +0300

    feat: add .gitignore & Main.java

--- src/Task1.java
+++ src/Task1.java
@@ -3,6 +3,5 @@ public class Task1 {
         System.out.println("Hello Dima!");
         System.out.println("NO conflict");
         System.out.println("Pull Push");
-        System.out.println("The end");
     }
 }

ed4a8c833750cc04861d1eae8144167dedd4b04e
Olga Komlik <oluska92666@gmail.com>
2022-09-21 12:44:48 +0300

    fixed file

--- src/Task1.java
+++ src/Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello Dima!");
-        System.out.println("Let's do it!");
+        System.out.println("Let's do tasks!");
     }
 }
